4821986f8bc54467f38f66706d73fe5eee7e3c1f
Akanksha <akankshaahire64@gmail.com>
2025-10-27 21:38:03 +0530

    Pushing today's Code

---
+++ CoreJava/src/com/todays/DuplicateArray.java
@@ -0,0 +1,25 @@
+package com.todays;
+
+import java.util.HashSet;
+
+public class DuplicateArray {
+public static void main(String[] args) {
+	String[] strArray = {"abc", "def", "abc", "mno", "xyz", "pqr", "xyz", "pqr"}; 
+	//1. Using Brute Force Method 
+	for (int i = 0; i < strArray.length-1; i++) 
+	{ 
+	for (int j = i+1; j < strArray.length; j++) 
+	{ 
+	if( (strArray[i]==(strArray[j])) ) 
+	{ 
+	System.out.println("Brute Force Method : Duplicate Element is : "+strArray[j]); 
+	}}} 
+	// using Hashset 
+	HashSet<String> hs = new HashSet<String>(); 
+	for (String arrayElement : strArray) 
+	{ 
+	if(!hs.add(arrayElement)) 
+	{System.out.println("HashSet :Duplicate Element is : "+arrayElement); 
+	}} 
+}
+}
---
+++ CoreJava/src/com/todays/OccurenceOfEachCharacter.java
@@ -0,0 +1,31 @@
+package com.todays;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class OccurenceOfEachCharacter {
+public static void main(String[] args) { 
+		String str = "This is an example"; 
+		HashMap<Character, Integer> count = new HashMap<Character, Integer>(); 
+		// convert string to character array 
+		char[] arr = str.toCharArray(); 
+		// traverse every character and count the Occurrences 
+		for (char c : arr) { 
+		// filter out white spaces 
+		if (c != ' ') { 
+		if (count.containsKey(c)) { 
+		// if character already traversed, increment it 
+		count.put(c, count.get(c) + 1); 
+		} else { 
+		// if character not traversed, add it to hashmap 
+		count.put(c, 1); 
+		} 
+		} 
+		} 
+		// traverse the map and print the number of occurences of a character 
+		for (Entry<Character, Integer> entry : count.entrySet()) { 
+		System.out.print( entry.getKey() + " = " + entry.getValue()+", "); 
+		} 
+		} 
+}
+
